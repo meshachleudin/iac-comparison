9adc3a4dca13b2ab053daefc781faffb1d6a080b
Meshach Leudin <leudin777@gmail.com>
2025-11-27 20:15:10 +0000

    Update outputs.tf

--- terraform/remote_state/outputs.tf
+++ terraform/remote_state/outputs.tf
@@ -1,77 +1,24 @@
-# outputs.tf
-output "vpc_id" {
-  value       = aws_vpc.main.id
-  description = "VPC id"
+output "s3_bucket_name" {
+  value       = aws_s3_bucket.terraform_state.id
+  description = "The NAME of the S3 bucket"
 }
 
-output "public_subnet_id" {
-  value       = aws_subnet.public.id
-  description = "Public subnet id"
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
 }
 
-output "private_subnet_id" {
-  value       = aws_subnet.private.id
-  description = "Private subnet id"
+output "s3_bucket_region" {
+  value       = aws_s3_bucket.terraform_state.region
+  description = "The REGION of the S3 bucket"
 }
 
-output "public_route_table_id" {
-  value       = aws_route_table.public.id
-  description = "Public route table id"
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_lock.name
+  description = "The ARN of the DynamoDB table"
 }
 
-output "private_route_table_id" {
-  value       = aws_route_table.private.id
-  description = "Private route table id"
-}
-
-output "internet_gateway_id" {
-  value       = aws_internet_gateway.igw.id
-  description = "Internet Gateway id"
-}
-
-output "nat_eip_allocation_id" {
-  value       = aws_eip.nat_allocation.id
-  description = "NAT EIP allocation id"
-}
-
-output "nat_gateway_id" {
-  value       = aws_nat_gateway.nat.id
-  description = "NAT Gateway id"
-}
-
-output "security_group_id" {
-  value       = aws_security_group.instance_sg.id
-  description = "Instance security group id"
-}
-
-output "public_instance_id" {
-  value       = aws_instance.public.id
-  description = "Public instance id"
-}
-
-output "public_instance_public_ip" {
-  value       = aws_instance.public.public_ip
-  description = "Public instance public ip"
-}
-
-output "private_instance_id" {
-  value       = aws_instance.private.id
-  description = "Private instance id"
-}
-
-output "private_instance_private_ip" {
-  value       = aws_instance.private.private_ip
-  description = "Private instance private ip"
-}
-
-output "vpc_id" {
-  value = aws_vpc.this.id
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private.id
+output "dynamodb_table_arn" {
+  value       = aws_dynamodb_table.terraform_lock.arn
+  description = "The ARN of the DynamoDB table"
 }
